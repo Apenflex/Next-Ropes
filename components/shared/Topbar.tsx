06846e48e40285a97173ee7634c081195dace0e8
Svyatoslav <blackdbf@gmail.com>
2023-08-15 17:15:25 +0300

    feat: add form

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,4 +1,5 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -21,6 +22,7 @@ const Topbar = () => {
 				</div>
 				<OrganizationSwitcher
 					appearance={{
+						baseTheme: dark,
 						elements: {
 							organizationSwitcherTrigger: 'py-2 px-4',
 						},
