646ca1829c62d2370fa1004a806d8c5c4be476e3
Svyatoslav <blackdbf@gmail.com>
2023-09-03 14:02:03 +0300

    feat: add comments

--- components/shared/RopesTab.tsx
+++ components/shared/RopesTab.tsx
@@ -1,5 +1,6 @@
 import { redirect } from 'next/navigation'
 
+import RopeCard from '@/components/cards/RopeCard'
 import { fetchUserPosts } from '@/lib/actions/user.actions'
 
 interface RopesTabProps {
@@ -12,7 +13,27 @@ const RopesTab = async ({ currentUserId, accountId, accoutType }: RopesTabProps)
 	let res = await fetchUserPosts(accountId)
 	if (!res) redirect('/')
 
-	return <section className="mt-9 flex flex-col gap-10">RopesTab</section>
+	return (
+		<section className="mt-9 flex flex-col gap-10">
+			{res.ropes.map((rope: any) => (
+				<RopeCard
+					key={rope._id}
+					id={rope.id}
+					currentUserId={currentUserId}
+					parentId={rope.parentId}
+					content={rope.text}
+					author={
+						accoutType === 'User'
+							? { name: res.name, image: res.image, id: res.id }
+							: { name: rope.author.name, image: rope.author.image, id: rope.author.id }
+					}
+					community={rope.community} // TODO:
+					createdAt={rope.createdAt}
+					comments={rope.children}
+				/>
+			))}
+		</section>
+	)
 }
 
 export default RopesTab
