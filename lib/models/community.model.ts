dbcd91f8fbc2c2c0ce3708170c67cbfe82b7b10b
Svyatoslav <blackdbf@gmail.com>
2023-09-04 11:35:57 +0300

    feat: add clerk webhook

---
+++ lib/models/community.model.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const communitySchema = new mongoose.Schema({
+	id: { type: String, required: true },
+	username: { type: String, required: true, unique: true },
+	name: { type: String, required: true },
+	image: { type: String },
+  bio: { type: String },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+	ropes: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Rope',
+		},
+	],
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    }
+  ],
+})
+
+const Community = mongoose.models.Community || mongoose.model('Community', communitySchema)
+
+export default Community
